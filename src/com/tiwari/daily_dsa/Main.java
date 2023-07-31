58ba452e8e0b3e05b2eeb6dc70de629fd4c26cef
tiwarichandra <chandra.tiwari@neosoftmail.com>
2023-07-31 13:05:03 +0530

    SameElementRearrangeFromArray problem

--- src/com/tiwari/daily_dsa/Main.java
+++ src/com/tiwari/daily_dsa/Main.java
@@ -1,3 +1,5 @@
+package com.tiwari.daily_dsa;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/com/tiwari/daily_dsa/SameElementRearrangeFromArray.java
@@ -0,0 +1,22 @@
+package com.tiwari.daily_dsa;
+
+import java.util.Arrays;
+
+public class SameElementRearrangeFromArray {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 5, 5, 3, 1, 2, 2, 4, 3 };
+//        int[] arr = { 1, 2, 3, 1, 2, 1 };
+        int length = arr.length;
+        for (int i = 0; i<length; i++) {
+            int temp = 0;
+            for (int j = i+1; j< length; j++) {
+                if (arr[i] == arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
