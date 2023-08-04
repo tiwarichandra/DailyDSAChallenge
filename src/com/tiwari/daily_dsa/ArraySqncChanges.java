92ce044cc088250e958c1e73c830038c39af2773
tiwarichandra <chandra.tiwari@neosoftmail.com>
2023-08-04 15:31:34 +0530

    pair of int array eq to some of target

--- src/com/tiwari/daily_dsa/ArraySqncChanges.java
+++ src/com/tiwari/daily_dsa/ArraySqncChanges.java
@@ -2,6 +2,24 @@ package com.tiwari.daily_dsa;
 
 import java.util.Arrays;
 
+/*
+* Given an array of distinct integers, shuffle it according to the given order of elements.
+For example:
+Input:
+nums[] = { 1, 2, 3, 4, 5 }
+pos[] = { 3, 2, 4, 1, 0 }
+
+Output:
+
+nums[] = { 5, 4, 2, 1, 3 }
+
+i.e., if pos[i] = j, then update nums[j] = nums[i] for every index i.
+In other words, update nums[pos[i]] = nums[i] for every index i.
+
+*
+* */
+
+
 public class ArraySqncChanges {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 4, 5 };
